47693a5e5893f9f6ff2add457fd99aa84179a746
Jhonatta <jhonattasantos27@gmaol.com>
2025-03-03 11:31:01 -0300

    chore: configure environment variables for sensitive data
    - add .env file to store database credentials
    
    - Update application.yml to use environment variables

--- src/main/java/com/jhonatta/CrudSpring/CrudSpringApplication.java
+++ src/main/java/com/jhonatta/CrudSpring/CrudSpringApplication.java
@@ -1,5 +1,6 @@
 package com.jhonatta.CrudSpring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
+		//Load the variables from the .env file.
+		Dotenv dotenv = Dotenv.configure().load();
+
+		//Set the environment variables in the system.
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
+		//Start SpringBoot Aplication
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
