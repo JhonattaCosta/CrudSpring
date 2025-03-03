dda34e957d037683abd183d8ccbbcdcb73194a07
Jhonatta <jhonattasantos27@gmaol.com>
2025-03-03 11:54:55 -0300

    feat: add PessoaModel Class with attributes and Spring annotations

--- src/main/java/com/jhonatta/CrudSpring/model/PessoaModel.java
+++ src/main/java/com/jhonatta/CrudSpring/model/PessoaModel.java
@@ -1,4 +1,34 @@
 package com.jhonatta.CrudSpring.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="cadastro_pessoa")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="cpf")
+    private String cpf;
+
+    @Column(unique = true, name="email")
+    private String email;
+
+    @Column(name="num_telefone")
+    private Long numeroTelefone;
+
+    @Column(name="data_nascimento")
+    private LocalDate dataNascimento;
 }
